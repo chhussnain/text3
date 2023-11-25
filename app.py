24bea9f73100fb4bd93fd08dd8635a5d07524e79
chhussnain <126756759+chhussnain@users.noreply.github.com>
2023-11-25 12:24:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,33 @@
 import streamlit as st
-from transformers import pipeline
 
-# Load pre-trained text classification model from Hugging Face
-classifier = pipeline("sentiment-analysis")
+positive_words = ["happy", "good", "excellent", "positive", "awesome"]
+negative_words = ["sad", "bad", "terrible", "negative", "awful"]
 
 def predict_sentiment(text):
-    result = classifier(text)
-    return result[0]
+    words = text.lower().split()
+    positive_count = sum(word in positive_words for word in words)
+    negative_count = sum(word in negative_words for word in words)
+
+    if positive_count > negative_count:
+        return "Positive"
+    elif negative_count > positive_count:
+        return "Negative"
+    else:
+        return "Neutral"
 
 def main():
-    st.title("Text Classification App")
+    st.title("Simple Sentiment Analysis App")
 
     # User input
-    text_input = st.text_area("Enter text for classification:")
+    text_input = st.text_area("Enter text for sentiment analysis:")
 
     if st.button("Predict"):
         if text_input:
             prediction = predict_sentiment(text_input)
-            st.write(f"Prediction: {prediction['label']} with confidence: {prediction['score']:.4f}")
+            st.write(f"Sentiment: {prediction}")
         else:
             st.warning("Please enter some text.")
 
 if __name__ == "__main__":
     main()
+
